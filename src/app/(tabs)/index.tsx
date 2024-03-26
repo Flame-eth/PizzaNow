5027e1c69d7f1f2ea429c4be356acda2278ee837
Flame-eth <theflamepen@gmail.com>
2024-03-26 03:56:10 +0100

    Fix styling in MenuScreen component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -13,7 +13,7 @@ export default function MenuScreen() {
       <FlatList data={products} 
         renderItem={({ item }) => <ProductListItem product={item} />}
         numColumns={2}
-        contentContainerStyle={{gap: 10, padding: 10}}
+        contentContainerStyle={{gap: 10, padding: 10 }}
         columnWrapperStyle={{ gap: 10}}
         keyExtractor={(item) => item.id.toString()} 
        />
